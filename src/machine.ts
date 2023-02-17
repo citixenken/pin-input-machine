ac4635404f092b8a1bfeff3d15ae9de237ae5da0
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2023-02-17 13:29:50 +0300

    implement state machine action for setting input focus in Zag.js

--- src/machine.ts
+++ src/machine.ts
@@ -11,29 +11,44 @@ type MachineState = {
   value: "idle" | "focused";
 };
 
-export const machine = createMachine<MachineContext, MachineState>({
-  id: "pin-input",
-  context: { value: [], focusedIndex: -1 },
-  initial: "idle",
-  states: {
-    idle: {
-      on: {
-        FOCUS: {
-          target: "focused",
-          actions: ["setFocusedIndex"],
+export const machine = createMachine<MachineContext, MachineState>(
+  {
+    id: "pin-input",
+    context: { value: [], focusedIndex: -1 },
+    initial: "idle",
+    states: {
+      idle: {
+        on: {
+          FOCUS: {
+            target: "focused",
+            actions: ["setFocusedIndex"],
+          },
         },
       },
-    },
-    focused: {
-      on: {
-        BLUR: {
-          target: "idle",
-          actions: ["clearFocusedIndex"],
+      focused: {
+        on: {
+          BLUR: {
+            target: "idle",
+            actions: ["clearFocusedIndex"],
+          },
+          INPUT: { actions: ["setFocusedValue", "focusNextInput"] },
+          BACKSPACE: { actions: ["clearFocusedValue", "focusPrevInput"] },
+          PASTE: { actions: ["setPastedValue", "focusLastEmptyInput"] },
         },
-        INPUT: { actions: ["setFocusedValue", "focusNextInput"] },
-        BACKSPACE: { actions: ["clearFocusedValue", "focusPrevInput"] },
-        PASTE: { actions: ["setPastedValue", "focusLastEmptyInput"] },
       },
     },
   },
-});
+  {
+    actions: {
+      setFocusedIndex(context, e) {
+        context.focusedIndex = e.idx;
+      },
+      clearFocusedIndex(context) {
+        context.focusedIndex = -1;
+      },
+      //   setFocusedValue() {},
+      //   clearFocusedValue() {},
+      //   setPastedValue() {},
+    },
+  }
+);
